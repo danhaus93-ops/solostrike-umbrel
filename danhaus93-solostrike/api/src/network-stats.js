97873a4b1316b56da432d35b833afae05a758a95
danhaus93-ops <danhaus93@icloud.com>
2026-04-26 08:06:28 -0500

    Update network-stats.js

--- danhaus93-solostrike/api/src/network-stats.js
+++ danhaus93-solostrike/api/src/network-stats.js
@@ -58,7 +58,7 @@ const DEFAULT_RELAYS = [
   'wss://nos.lol',
   'wss://relay.primal.net',
   'wss://nostr.mom',
-  'wss://offchain.pub',
+  'wss://nostr.fmt.wiz.biz',
   'wss://nostr.bitcoiner.social',
   'wss://nostr.oxtr.dev',
   'wss://nostr-pub.wellorder.net',
