ccc629b281ce874662291c81092361cd5199de1d
danhaus93-ops <danhaus93@icloud.com>
2026-05-03 19:42:28 -0500

    Add files via upload

--- danhaus93-solostrike/ui/public/sw.js
+++ danhaus93-solostrike/ui/public/sw.js
@@ -7,7 +7,7 @@
 
 // Build marker: 1.8.3-rev22 (impliedHr uses session-scoped acceptedSdiffSum, not lifetime ckpool sum)
 
-const CACHE_NAME = 'solostrike-v1.8.8-globe-rev7';
+const CACHE_NAME = 'solostrike-v1.8.8-globeglyph-rev8';
 const SHELL_ASSETS = [
   '/',
   '/index.html',
