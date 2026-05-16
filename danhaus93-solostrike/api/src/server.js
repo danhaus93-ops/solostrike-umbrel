610dc3f3a740523bced3f7712bfbfefe9d493852
danhaus93-ops <danhaus93@icloud.com>
2026-05-16 00:50:50 -0500

    Add files via upload

--- danhaus93-solostrike/api/src/server.js
+++ danhaus93-solostrike/api/src/server.js
@@ -1,4 +1,4 @@
-// SoloStrike API server (v1.11.11 — privacy-aware)
+// SoloStrike API server (v1.11.12 — privacy-aware)
 const fs = require('fs-extra');
 const path = require('path');
 const express = require('express');
